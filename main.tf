d087ad96d51e99e030cd7c6de02f5816e28df55f
Austin Workman <austin@hashicorp.com>
2019-03-06 10:24:12 -0600

    Moving instance count to a static value

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "compute_instance" {
   source  = "app.terraform.io/aworkman/compute-instance/google"
   version = "0.1.4"
 
-  count = "${var.compute_instance_count}"
+  count = "1"
   disk_image = "${var.compute_instance_disk_image}"
   disk_size = "${var.compute_instance_disk_size}"
   machine_type = "${var.compute_instance_machine_type}"
